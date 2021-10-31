5fa73b2a6010798a6e3ad8669c40750036e44402
Foebry <rain_fabry@hotmail.com>
2021-10-31 10:57:26 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+import os
+
+from codecs import open
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+packages = ['requesting']
+
+requires = []
+
+about = {}
+with open(os.path.join(here, 'requesting', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=packages,
+    package_data={'': ['LICENSE', 'NOTICE']},
+    package_dir={'requesting': 'requesting'},
+    include_package_data=True,
+    install_requires=requires,
+    license=about['__license__'],
+    project_urls={
+        'Source': 'https://github.com/Foebry/requesting',
+    },
+)
